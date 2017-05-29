bbf678aa0e15e74529e33f7b0f023d2141d18fb4
QP <qp@powerme.com.sg>
2017-05-29 21:36:27 +0800

    init

---
+++ data/phrases/allow.js
@@ -0,0 +1,9 @@
+var allow = [
+    {
+        verb: "allow",
+        suffix: "for sth",
+        full: "allow for sth",
+        meaning: "to consider or include something when you are making plans or judging a situation",
+        example: "the dissident was allowed to leave the country"
+    },
+]
---
+++ data/phrases/answer.js
@@ -0,0 +1,9 @@
+var answer = [
+    {
+        verb: "answer",
+        suffix: "(sb) back",
+        full: "answer (sb) back",
+        meaning: "if someone, especially a child, answers back, or answers someone back, they reply rudely to someone they should be polite to",
+        example: "the dissident was allowed to leave the country"
+    },
+]
